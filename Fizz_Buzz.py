b7323e1dbfa702446b924ac6fa4895f66ae7df34
sergiomendozer <sergiomendozansx@gmail.com>
2022-01-12 14:31:19 -0800

    Fizz Buzz old project added

--- Fizz_Buzz.py
+++ Fizz_Buzz.py
@@ -1,1 +1,9 @@
-#ffiiz
+for n in range(0,21):
+    if n%3==0 and n%5!=0:
+        print ("Fizz")
+    elif n%5==0 and n%3!=0:
+        print ("Buzz")
+    elif n%3==0 and n%5==0:
+        print ("FizzBuzz")
+    elif n%3!=0 and n%5!=0:
+        print (n)
