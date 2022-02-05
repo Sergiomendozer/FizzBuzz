23f0d31119b2e23c639b278e515abfab4854379a
sergiomendozer <sergiomendozansx@gmail.com>
2022-02-05 00:22:21 -0800

    reformatted

--- Fizz_Buzz.py
+++ Fizz_Buzz.py
@@ -1,9 +1,9 @@
-for n in range(0,21):
-    if n%3==0 and n%5!=0:
-        print ("Fizz")
-    elif n%5==0 and n%3!=0:
-        print ("Buzz")
-    elif n%3==0 and n%5==0:
-        print ("FizzBuzz")
-    elif n%3!=0 and n%5!=0:
-        print (n)
+for n in range(0, 21):
+    if n % 3 == 0 and n % 5 != 0:
+        print("Fizz")
+    elif n % 5 == 0 and n % 3 != 0:
+        print("Buzz")
+    elif n % 3 == 0 and n % 5 == 0:
+        print("FizzBuzz")
+    elif n % 3 != 0 and n % 5 != 0:
+        print(n)
